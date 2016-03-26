ae17cd520d55b6556ef1df683268bb8302fabd91
osmanf <osman.findik@telenity.com>
2016-03-27 01:47:40 +0200

    Second parts of day1 and day2 implemented

--- day1/main.go
+++ day1/main.go
@@ -8,13 +8,21 @@ import (
 func main() {
 	direction := os.Args[1]
 	curFloor := 0
+	pos := 0
+	firstBasementPos := 0
 	for i := 0; i < len(direction); i++ {
 		if direction[i] == '(' {
 			curFloor += 1
 		} else if direction[i] == ')' {
 			curFloor += -1
 		}
+		pos += 1
+		if curFloor == -1 && firstBasementPos == 0 {
+			firstBasementPos = pos
+		}
+
 	}
 	fmt.Println("Floor:", curFloor)
+	fmt.Println("firstBasementPos:", firstBasementPos)
 
 }
--- day2/main.go
+++ day2/main.go
@@ -2,34 +2,61 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
 	"sort"
 	"strconv"
+	"strings"
 )
 
-func main() {
-	l := parseArg(os.Args[1])
-	w := parseArg(os.Args[2])
-	h := parseArg(os.Args[3])
-	area := (2 * l * w) + (2 * l * h) + (2 * h * w)
-	fmt.Println("area:", area)
-	dimension := []int{
-		l,
-		w,
-		h,
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
-	sort.Ints(dimension)
-	fmt.Println("dimension:", dimension)
-	extra := dimension[0] * dimension[1]
-	fmt.Println("extra:", extra)
-	total := area + extra
-	fmt.Println("total:", total)
 }
 
-func parseArg(arg string) int {
+func convertToInt(arg string) int {
 	result, err := strconv.Atoi(arg)
 	if err != nil {
 		panic(err)
 	}
 	return result
+}
+
+func getPaperAndRibbon(dimension string) (int, int) {
+	if dimension == "" {
+		return 0, 0
+	}
+	dimensions := strings.Split(dimension, "x")
+	fmt.Println(dimensions)
+	l := convertToInt(dimensions[0])
+	w := convertToInt(dimensions[1])
+	h := convertToInt(dimensions[2])
+	area := (2 * l * w) + (2 * l * h) + (2 * h * w)
+	fmt.Println("area:", area)
+	dimensionSlice := []int{
+		l,
+		w,
+		h,
+	}
+	sort.Ints(dimensionSlice)
+	extra := dimensionSlice[0] * dimensionSlice[1]
+	paper := area + extra
+	ribbon := dimensionSlice[0] + dimensionSlice[0] + dimensionSlice[1] + dimensionSlice[1]
+	bow := l * w * h
+	ribbon = ribbon + bow
+	return paper, ribbon
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("./test.txt")
+	check(err)
+	dataStr := string(dat)
+	dataArr := strings.Split(dataStr, "\n")
+	totalPaper, totalRibbon := 0, 0
+	for i := 0; i < len(dataArr); i++ {
+		paper, ribbon := getPaperAndRibbon(dataArr[i])
+		totalPaper += paper
+		totalRibbon += ribbon
+	}
+	fmt.Println("totalPaper:", totalPaper, " totalRibbon:", totalRibbon)
 }
